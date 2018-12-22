43ce423bc89d89489c249e6e167e898e69d74e65
D0ub1ePieR <535447508@qq.com>
2018-12-23 04:39:03 +0800

    add 实现多客户端ping多内部主机

--- client.py
+++ client.py
@@ -1,9 +1,19 @@
 from scapy.all import *
 
 pkt_data=[]
-ip_table=["192.168.108.137"]
+ip_table=["192.168.108.137","192.168.108.1"]
 server_ip_out="192.168.160.129"
 server_ip_in="192.168.108.128"
+
+def two_length_str(pstr):
+	'''
+		param : string
+		return : length of string (length 2)
+	'''
+	tstr=str(len(pstr))
+	if len(tstr)<2:
+		tstr='0'+tstr
+	return tstr
 
 def fix_icmp(pktx):
 	'''
@@ -12,7 +22,7 @@ def fix_icmp(pktx):
 	try:
 		proto=pktx[IP].proto
 	except:
-		print("[log] not icmp packet") # TODO
+		print("[log] not icmp packet") #TODO
 	else:
 		src_ip=pktx[IP].src
 		dst_ip=pktx[IP].dst
@@ -20,17 +30,20 @@ def fix_icmp(pktx):
 		if proto==1:
 			pkt_id=pktx[ICMP].id
 			pkt_seq=pktx[ICMP].seq
+			pkt_icmp_data=pktx[ICMP].payload.load
 			print("[log] catch an icmp paclet : ip.src",src_ip," ip.dst",dst_ip," icmp.seq",pkt_seq," icmp.id",pkt_id)
 			#目标ip在虚拟局域网内将报文发送给服务器
 			if dst_ip in ip_table:
-				send(IP(dst=server_ip_out)/ICMP(seq=pkt_seq+50,id=pkt_id+50)/Raw(pktx[ICMP].payload.load+Raw(dst_ip).load))
+				#拼接数据段 记录目标ip
+				send_payload=pkt_icmp_data+Raw(dst_ip).load+Raw(two_length_str(dst_ip)).load
+				send(IP(dst=server_ip_out)/ICMP(seq=pkt_seq+50,id=pkt_id+50)/Raw(send_payload))
 
 def sniff_packet():
 	'''
 		抓取本机发送的数据包
 	'''
 	# sniff(filter,iface,prn,count)
-	sniff(filter="icmp",prn=fix_icmp)
+	sniff(iface="ens38",prn=fix_icmp)
 
 
 def main():
--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from scapy.all import *
 
 client_ip_table=["192.168.160.128"]
-target_ip="192.168.108.137"
+target_ip_table=["192.168.108.137","192.168.108.1"]
 server_ip_out="192.168.160.129"
 server_ip_in="192.168.108.128"
+request_stack=[]
 
 def transmit_icmp(pktx):
     '''
@@ -15,18 +16,31 @@ def transmit_icmp(pktx):
 		print("[log] not icmp packet") # TODO
     else:
         src_ip=pktx[IP].src
-		dst_ip=pktx[IP].dst
+        dst_ip=pktx[IP].dst
         #proto为1 表示icmp
-		if proto==1:
-			pkt_id=pktx[ICMP].id
-			pkt_seq=pktx[ICMP].seq
+        if proto==1:
+            pkt_id=pktx[ICMP].id
+            pkt_seq=pktx[ICMP].seq
+            pkt_icmp_data=pktx[ICMP].payload.load
 			print("[log] catch an icmp paclet : ip.src",src_ip," ip.dst",dst_ip," icmp.seq",pkt_seq," icmp.id",pkt_id)
-			if src_ip in client_ip_table and dst_ip==server_ip_out:
+            if src_ip in client_ip_table and dst_ip==server_ip_out:
+                #ICMP type为8 表示request请求报文
                 if pktx[ICMP].type==8:
-                    send(IP(dst=target_ip)/ICMP(seq=pkt_seq,id=pkt_id)/pktx[ICMP].payload)
-            elif src_ip==target_ip and dst_ip!=client_ip_table[0]:
+                    ip_length=int(pkt_icmp_data[-2:])
+                    request_ip=pkt_icmp_data[-2-ip_length:-2]
+                    request_stack.append([src_ip,request_ip])
+                    send(IP(src=server_ip_in,dst=request_ip)/ICMP(seq=pkt_seq,id=pkt_id)/pktx[ICMP].payload)
+            elif src_ip in target_ip_table and dst_ip not in client_ip_table:
+                #ICMP type为0 表示reply回复报文
                 if pktx[ICMP].type==0:
-                    send(IP(src=target_ip,dst=client_ip_table[0])/ICMP(seq=pkt_seq-50,type=0,id=pkt_id-50)/pktx[ICMP].payload);
+                    for pair in request_stack:
+                        index=-1
+                        if pair[1]==Raw(src_ip).load:
+                            index=request_stack.index(pair)
+                            send(IP(src=pair[1],dst=pair[0])/ICMP(seq=pkt_seq-50,type=0,id=pkt_id-50)/pktx[ICMP].payload);
+                        if index!=-1:
+                            request_stack.remove(request_stack[index])
+                            break
 
 def sniff_packet():
 	'''
