bd85f03684d89d17b9e11c782f21891402ad25c7
D0ub1ePieR <535447508@qq.com>
2018-12-24 18:15:43 +0800

    add tcp三次握手 fix 将所有tab用四个空格代替

--- client.py
+++ client.py
@@ -3,80 +3,90 @@ from scapy.all import *
 pkt_data=[]
 ip_table=["192.168.108.137","192.168.108.1"]
 server_ip_out="192.168.160.129"
-server_ip_in="192.168.108.128"
+#server_ip_in="192.168.108.128"
 
 def two_length_str(pstr):
-	'''
-		param : string
-		return : length of string (length 2)
-	'''
-	tstr=str(len(pstr))
-	if len(tstr)<2:
-		tstr='0'+tstr
-	return tstr
+    '''
+        param : string
+        return : length of string (length 2)
+    '''
+    tstr=str(len(pstr))
+    if len(tstr)<2:
+        tstr='0'+tstr
+    return tstr
 
 def fix_icmp(pktx):
-	'''
-		处理icmp
-	'''
-	try:
-		proto=pktx[IP].proto
-	except:
-		print("[log] not ip packet") #TODO
-	else:
-		src_ip=pktx[IP].src
-		dst_ip=pktx[IP].dst
-		#proto为1 表示icmp
-		if proto==1:
-			pkt_id=pktx[ICMP].id
-			pkt_seq=pktx[ICMP].seq
-			pkt_icmp_data=pktx[ICMP].payload.load
-			print("[log] catch an icmp paclet : ip.src",src_ip," ip.dst",dst_ip," icmp.seq",pkt_seq," icmp.id",pkt_id)
-			#目标ip在虚拟局域网内将报文发送给服务器
-			if dst_ip in ip_table:
-				#拼接数据段 记录目标ip
-				send_payload=pkt_icmp_data+Raw(dst_ip).load+Raw(two_length_str(dst_ip)).load
-				send(IP(dst=server_ip_out)/ICMP(seq=pkt_seq+50,id=pkt_id+50)/Raw(send_payload))
+    '''
+        处理icmp
+    '''
+    try:
+        proto=pktx[IP].proto
+    except:
+        print("[log] not ip packet") #TODO
+    else:
+        src_ip=pktx[IP].src
+        dst_ip=pktx[IP].dst
+        #proto为1 表示icmp
+        if proto==1:
+            pkt_id=pktx[ICMP].id
+            pkt_seq=pktx[ICMP].seq
+            pkt_icmp_data=pktx[ICMP].payload.load
+            print("[log] catch an icmp paclet : ip.src",src_ip," ip.dst",dst_ip," icmp.seq",pkt_seq," icmp.id",pkt_id)
+            #目标ip在虚拟局域网内将报文发送给服务器
+            if dst_ip in ip_table:
+                #拼接数据段 记录目标ip
+                send_payload=pkt_icmp_data+Raw(dst_ip).load+Raw(two_length_str(dst_ip)).load
+                send(IP(dst=server_ip_out)/ICMP(seq=pkt_seq+50,id=pkt_id+50)/Raw(send_payload))
 
 def fix_tcp(pktx):
-	'''
-		处理tcp包
-	'''
-	src_ip=pktx[IP].src
-	dst_ip=pktx[IP].dst
-	try:
-		pkt_id=pktx[TCP].id
-		pkt_seq=pktx[TCP].seq
-		pkt_flags=pktx[TCP].flags
-		pkt_sport=pktx[TCP].sport
-		pkt_dport=pktx[TCP].dport
-	except:
-		print("[log] tcp pakcet fix failed")
-	else:
-		#TCP flags FIN-1 SYN-2 RST-4 ACK-16
-
+    '''
+        处理tcp包
+    '''
+    src_ip=pktx[IP].src
+    dst_ip=pktx[IP].dst
+    try:
+        pkt_ack=pktx[TCP].ack
+        pkt_seq=pktx[TCP].seq
+        pkt_flags=pktx[TCP].flags
+        pkt_sport=pktx[TCP].sport
+        pkt_dport=pktx[TCP].dport
+    except:
+        print("[log] tcp pakcet fix failed")
+    else:
+        #TCP flags FIN-1 SYN-2 RST-4 ACK-16
+        if dst_ip in ip_table:
+            if pkt_flags==2:
+                send(IP(dst=server_ip_out)/TCP(flags=2,sport=pkt_sport,dport=pkt_dport,seq=pkt_seq,ack=pkt_ack)/Raw(dst_ip))
+            # 提供服务器转发的syn包会使服务器回复一个rst
+            if pkt_flags==4:
+                print("[log] recieve a server RST")
+            if pkt_flags==16:
+                send(IP(dst=server_ip_out)/TCP(flags=16,sport=pkt_sport,dport=pkt_dport,seq=pkt_seq,ack=pkt_ack)/Raw(dst_ip))
+            # 理论上不会进这个分支
+            if pkt_flags==18:
+                send(IP(dst=server_ip_out)/TCP(flags=18,sport=pkt_sport,dport=pkt_dport,seq=pkt_seq))
 
 def sniff_packet():
-	'''
-		抓取本机发送的数据包
-	'''
-	# sniff(filter,iface,prn,count)
-	def classify(pktx):
-		'''
-			sniff回调处理函数,分类包
-		'''
-		try:
-			proto=pktx[IP].proto
-		except:
-			print("[log] not ip packet")
-		else:
-			#proto为6 表示tcp
-			if proto==6:
-				fix_tcp(pktx)
-			#proto为1 表示icmp
-			if proto==1:
-				fix_icmp(pktx)
-	sniff(iface="ens38",prn=classify)
+    '''
+        抓取本机发送的数据包
+    '''
+    # sniff(filter,iface,prn,count)
+    def classify(pktx):
+        '''
+            sniff回调处理函数,分类包
+        '''
+        try:
+            proto=pktx[IP].proto
+        except:
+            print("[log] not ip packet")
+        else:
+            #proto为6 表示tcp
+            if proto==6:
+                fix_tcp(pktx)
+            #proto为1 表示icmp
+            if proto==1:
+                fix_icmp(pktx)
+    sniff(iface="ens38",prn=classify)
 
 
 def main():
--- server.py
+++ server.py
@@ -4,16 +4,17 @@ client_ip_table=["192.168.160.128"]
 target_ip_table=["192.168.108.137","192.168.108.1"]
 server_ip_out="192.168.160.129"
 server_ip_in="192.168.108.128"
+server_port=20000
 request_stack=[]
 
 def transmit_icmp(pktx):
     '''
-        转发报文
+        转发icmp报文
     '''
     try:
         proto=pktx[IP].proto
     except:
-		print("[log] not ip packet") # TODO
+        print("[log] not ip packet") # TODO
     else:
         src_ip=pktx[IP].src
         dst_ip=pktx[IP].dst
@@ -22,7 +23,7 @@ def transmit_icmp(pktx):
             pkt_id=pktx[ICMP].id
             pkt_seq=pktx[ICMP].seq
             pkt_icmp_data=pktx[ICMP].payload.load
-			print("[log] catch an icmp paclet : ip.src",src_ip," ip.dst",dst_ip," icmp.seq",pkt_seq," icmp.id",pkt_id)
+            print("[log] catch an icmp paclet : ip.src",src_ip," ip.dst",dst_ip," icmp.seq",pkt_seq," icmp.id",pkt_id)
             if src_ip in client_ip_table and dst_ip==server_ip_out:
                 #ICMP type为8 表示request请求报文
                 if pktx[ICMP].type==8:
@@ -42,12 +43,68 @@ def transmit_icmp(pktx):
                             request_stack.remove(request_stack[index])
                             break
 
+def transmit_tcp(pktx):
+    '''
+        实现客户端与目标主机的tcp连接
+    '''
+    src_ip=pktx[IP].src
+    dst_ip=pktx[IP].dst
+    try:
+        pkt_ack=pktx[TCP].ack
+        pkt_seq=pktx[TCP].seq
+        pkt_flags=pktx[TCP].flags
+        pkt_sport=pktx[TCP].sport
+        pkt_dport=pktx[TCP].dport
+    except:
+        print("[log] tcp pakcet fix failed")
+    else:
+        #TCP flags FIN-1 SYN-2 RST-4 ACK-16
+        if src_ip in client_ip_table and dst_ip==server_ip_out:
+            # SYN
+            if pkt_flags==2:
+                data=pktx.payload.load
+                if [src_ip,data,pkt_sport,pkt_dport] not in [t[:4] for t in request_stack]:
+                    # 记录每一个tcp连接
+                    request_stack.append([src_ip,data,pkt_sport,pkt_dport,server_port,pkt_seq,pkt_ack])
+                    server_port=server_port+1
+                    # 转发syn请求
+                    send(IP(src=src_ip,dst=data)/TCP(flags=2,sport=pkt_sport,dport=pkt_dport,seq=pkt_seq,ack=pkt_ack))
+            # ACK
+            if pkt_flags==16:
+                for i in len(request_stack):
+                    if request_stack[i][4]==[src_ip,dst_ip,pkt_sport,pkt_dport]:
+                        if pkt_ack==request_stack[i][5]+1 and pkt_seq==request_stack[i][6]:
+                            send(IP(src=src_ip,dst=pktx.payload.load)/TCP(flags=16,sport=pkt_sport,dport=pkt_dport,seq=pkt_seq,ack=pkt_ack))
+        if dst_ip in client_ip_table and src_ip!=server_ip_out:
+            if pkt_flags==18:
+                for i in len(request_stack):
+                    if request_stack[i][:4]==[dst_ip,src_ip,pkt_dport,pkt_sport]:
+                        if pkt_ack=request_stack[i][5]+1:
+                            send(IP(src=src_ip,dst=dst_ip)/TCP(flags=18,sport=pkt_sport,dport=pkt_dport,seq=pkt_seq,ack=pkt_ack))
+                            request_stack[i][5]=pkt_seq
+                            request_stack[i][6]=pkt_ack
+
 def sniff_packet():
-	'''
-		抓取本机发送的数据包
-	'''
-	# sniff(filter,iface,prn,count)
-	sniff(filter="icmp",prn=transmit_icmp)
+    '''
+        抓取本机发送的数据包
+    '''
+    def classify(pktx):
+        '''
+            sniff回调处理函数,分类包
+        '''
+        try:
+            proto=pktx[IP].proto
+        except:
+            print("[log] not ip packet")
+        else:
+            #proto为6 表示tcp
+            if proto==6:
+                transmit_tcp(pktx)
+            #proto为1 表示icmp
+            if proto==1:
+                transmit_icmp(pktx)
+    # sniff(filter,iface,prn,count)
+    sniff(filter="icmp",prn=classify)
 
 def main():
     '''
